08555a50d86d55801207b3f0f7ff17061754866c
Tomasz Slocinski <101492Tomasz@gmail.com>
2025-04-24 14:32:57 +0200

    Obsluga api z przegladarki

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware  # Import CORSMiddleware
 from predict import predict_image
 from PIL import Image
 import io
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],  # Dodaj adres frontendu
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Update the response to include both the brand and confidence percentage
 @app.post("/predict")
